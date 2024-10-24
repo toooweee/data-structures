89694f9a2b389a4ae15010f3374af197dc3b9edc
GM 52. Office <byprob03@gmail.com>
2024-10-24 17:00:04 +0500

    feat: (stack)

--- src/stack/stack.java
+++ src/stack/stack.java
@@ -1,7 +1,26 @@
 package stack;
 
+import java.util.Stack;
+
 public class stack {
     public static void main(String[] args) {
-        System.out.println(1234);
+        /*
+         * stack = LIFO data structure. Last-In First-Out
+         *         stores objects into a sort of "vertical tower"
+         *         push() to add to the top
+         *         pop() to remove from the top
+         */
+
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("Minecraft");
+        stack.push("Skyrim");
+        stack.push("DOOM");
+        stack.push("Borderlands");
+        stack.push("FFVII");
+
+        for(int i = 0; i < 10000000; i++) {
+            stack.push("Fallout 76");
+        }
     }
 }
